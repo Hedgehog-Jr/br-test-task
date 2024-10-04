29d7fd8daec32d86ef1e41610b7ff039c661dbab
pavel_ozhych <pavel_ozhych@epam.com>
2024-10-04 16:49:11 +0200

    Set default values for terraform variables with the key paths

--- infra/common.tfvars
+++
@@ -1,3 +0,0 @@
-client_certificate_path     = "/root/.minikube/profiles/minikube/client.crt"
-client_key_path             = "/root/.minikube/profiles/minikube/client.key"
-cluster_ca_certificate_path = "/root/.minikube/ca.crt"
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,13 +1,16 @@
 variable "client_certificate_path" {
   type = string
+  default = "/root/.minikube/profiles/minikube/client.crt"
 }
 
 variable "client_key_path" {
   type = string
+  default = "/root/.minikube/profiles/minikube/client.key"
 }
 
 variable "cluster_ca_certificate_path" {
   type = string
+  default = "/root/.minikube/ca.crt"
 }
 
 variable "kubernetes_host" {
