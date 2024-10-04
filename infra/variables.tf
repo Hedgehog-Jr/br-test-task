0e965d8eab0f739886877136089ec9298560d8d6
pavel_ozhych <pavel_ozhych@epam.com>
2024-10-04 16:54:02 +0200

    Add missed descriptions to terraform variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,18 +1,34 @@
 variable "client_certificate_path" {
   type = string
+  description = "Path to the client certificate file"
   default = "/root/.minikube/profiles/minikube/client.crt"
 }
 
 variable "client_key_path" {
   type = string
+  description = "Path to the client key file"
   default = "/root/.minikube/profiles/minikube/client.key"
 }
 
 variable "cluster_ca_certificate_path" {
   type = string
+  description = "Path to the cluster CA certificate file"
   default = "/root/.minikube/ca.crt"
+}
+
+variable "backend_organization" {
+  type = string
+  description = "Name of the Hashicorp organization"
+  default = "hedgehog_test_organization"
+}
+
+variable "backend_workspaces" {
+  type = string
+  description = "Name of the Hashicorp workspace"
+  default = "hedgehog_test_workspace"
 }
 
 variable "kubernetes_host" {
   type = string
+  description = "Kubernetes host address"
 }
